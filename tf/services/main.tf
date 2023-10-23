fbf018360a47c123dc4340a58bcfb394943981e9
N Linkov <nikilinkov@gmail.com>
2023-10-23 12:46:53 +0100

    move frontend container creation after app services in the creation order

--- tf/services/main.tf
+++ tf/services/main.tf
@@ -9,21 +9,3 @@ resource "vault_audit" "audit" {
 resource "vault_auth_backend" "userpass" {
   type     = "userpass"
 }
-
-resource "docker_container" "frontend" {
-  image = "docker.io/nginx:${var.nginx_version}"
-  name  = "frontend_${var.env_name}"
-
-  ports {
-    internal = 80
-    external = var.nginx_port
-  }
-
-  networks_advanced {
-    name = "vagrant_${var.env_name}"
-  }
-
-  lifecycle {
-    ignore_changes = all
-  }
-}
--- tf/services/services.tf
+++ tf/services/services.tf
@@ -8,3 +8,21 @@ module "service" {
   service_db_password = element(var.services_inputs, count.index)["service_db_password"]
   service_password = element(var.services_inputs, count.index)["service_password"]
 }
+
+resource "docker_container" "frontend" {
+  image = "docker.io/nginx:${var.nginx_version}"
+  name  = "frontend_${var.env_name}"
+
+  ports {
+    internal = 80
+    external = var.nginx_port
+  }
+
+  networks_advanced {
+    name = "vagrant_${var.env_name}"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
